d06d9b0246ce1c584238a7652e6da19f469f462a
Jin Choi <jinchoi@ucdavis.edu>
2017-07-27 16:40:38 -0700

    scraper seemingly done. testing from 2 levels deep

--- server/controllers/scraper.js
+++ server/controllers/scraper.js
@@ -22,7 +22,7 @@ function scrape($) {
     const price = Number($(elem).find('.p13n-sc-price').text().slice(1));
 
     if (price >= 10 && price <= 50) {
-      pending += 1;
+      console.log('pending: ', pending += 1);
       const prodName = cleanUp($(elem).find('.p13n-sc-truncated-hyphen').text());
       const imgLink = $(elem).find('img').attr('src');
       const listingUrl = `https://www.amazon.com${$(elem).find('a').attr('href')}`;
@@ -32,38 +32,47 @@ function scrape($) {
           (err, response, body) => {
             const $ = cheerio.load(body);
             const info = $('.prodDetTable tr');
-            let weight = cleanUp($(info).eq(1).find('td').text());
-            const split = weight.split(' ');
-
-            if (split[1].indexOf('pounds') > -1) {
-              if (Number(split[0]) <= 4) {
-                weight = `${split[0]} ${split[1]}`;
-              } else {
-                pending--;
-                return;
-              }
-            } else {
-              weight = `${split[0]} ${split[1]}`;
-            }
 
             let rank;
+            let dimensions;
+            let weight;
+
             $(info).each((i, elem) => {
               const name = $(elem).find('th').text();
               if (name.indexOf('Rank') > -1) {
-                rank = cleanUp($(elem).find('td').text().split('\\n')[0]);
+                rank = $(elem).find('td').text();
+                rank = cleanUp(rank.slice(0, rank.indexOf('(')));
+              } else if (name.indexOf('Dimensions') > -1) {
+                dimensions = cleanUp($(elem).find('td').text());
+              } else if (name.indexOf('Shipping Weight') > -1) {
+                weight = cleanUp($(elem).find('td').text());
+                const split = weight.split(' ');
+
+                if (split[1].indexOf('pounds') > -1) {
+                  if (Number(split[0]) <= 4) {
+                    weight = `${split[0]} ${split[1]}`;
+                  } else {
+                    pending--;
+                    weight = false;
+                  }
+                } else {
+                  weight = `${split[0]} ${split[1]}`;
+                }
               }
             });
-            const dimensions = cleanUp($(info).eq(0).find('td').text());
-
-            items.push(new Item(
-              listingUrl,
-              prodName,
-              price,
-              weight,
-              dimensions,
-              rank,
-              imgLink));
-            if (!--pending) {
+            if (weight !== false) {
+              pending--;
+              items.push(new Item(
+                listingUrl,
+                prodName,
+                price,
+                weight,
+                dimensions,
+                rank,
+                imgLink));
+            }
+            console.log(pending);
+            if (!pending) {
               fs.writeFile(path.join(__dirname, '../../data/data'), JSON.stringify(items, null, 2));
             }
           });
@@ -73,47 +82,48 @@ function scrape($) {
   });
 }
 
-request('https://www.amazon.com/Best-Sellers-Arts-Crafts-Sewing-Beading-Supplies/zgbs/arts-crafts/8090707011/ref=zg_bs_nav_ac_2_12896081',
-  (err, response, body) => {
-    scrape(cheerio.load(body));
-  });
+// request('https://www.amazon.com/Best-Sellers-Arts-Crafts-Sewing-Beading-Supplies/zgbs/arts-crafts/8090707011/ref=zg_bs_nav_ac_2_12896081',
+//   (err, response, body) => {
+//     const $ = cheerio.load(body);
+//     scrape($);
+//     iteratePagination($);
+//   });
 
-function iteratePagination($, elem) {
-  const callScraper = () => {
+function iteratePagination($) {
+  const callScraper = ($, elem) => {
     const url = $(elem).attr('href');
     request(url, (err, response, body) => {
       scrape(cheerio.load(body));
     });
   };
-  $(elem).find('.zg_pagination a').each((i, elem) => {
+  $('.zg_pagination a').each((i, elem) => {
     if (i !== 0) {
       setTimeout(callScraper.bind(null, $, elem), timer += randTime());
     }
   });
 }
 
-function digDown(url, count = 2) {
+function digDown(url, count = 3) {
   // get list of li links
   const ul = ' ul'.repeat(count);
-
+  console.log(url);
   request(url,
     (err, response, body) => {
       const $ = cheerio.load(body);
-      const list = $(`#zg_browseRoot${ul}`).find('li');
-
+      const list = $(`#zg_browseRoot${ul} a`);
       if (list.first().find('span').length === 1) {
         list.each((i, elem) => {
           // for first element just scrape
           const callScraper = ($, elem) => {
             if (i === 0) {
               scrape($);
-              iteratePagination($, elem);
+              iteratePagination($);
             } else {
               // 2nd and on follow link for next list item and pass into scraper
               request($(elem).find('a').attr('href'),
                 (err, response, body) => {
                   scrape(cheerio.load(body));
-                  iteratePagination($, elem);
+                  iteratePagination($);
                 });
             }
           };
@@ -121,8 +131,9 @@ function digDown(url, count = 2) {
         });
       } else {
         list.each((i, elem) => {
+          console.log($(elem).text());
           const timedOut = ($, elem) => {
-            digDown($(elem).find('a').attr('href'), count + 1);
+            digDown($(elem).attr('href'), count + 1);
           };
           setTimeout(timedOut.bind(null, $, elem), timer += randTime());
         });
@@ -132,7 +143,7 @@ function digDown(url, count = 2) {
   // call digDown on every link if not
   // call scrape on every link if so
 }
-
+digDown('https://www.amazon.com/Best-Sellers-Arts-Crafts-Sewing-Beading-Jewelry-Making/zgbs/arts-crafts/12896081/ref=zg_bs_unv_ac_2_12896121_1');
 
 const scrapeBestSellersList = () => {
   request('https://www.amazon.com/Best-Sellers/zgbs/ref=zg_bsms_tab',
